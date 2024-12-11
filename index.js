bacb7d442b52b0415ba0b99934afb8d3383eeaf4
SAYAN <sayadey1203@gmail.com>
2024-12-11 21:53:27 +0530

    add basic boilerplate

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const mongoose=require("mongoose");
 const listing=require("./models/listing");
 const path=require("path");
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
 
 main()
 .then(()=>{
